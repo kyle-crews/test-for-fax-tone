63a5c66394dc437789b5c19adc648b13436e9dd7
Kyle Crews <kyle.crews@kyle-crews.attlocal.net>
2021-06-04 00:55:09 -0500

    reverted back to call request

--- run_this.rb
+++ run_this.rb
@@ -6,13 +6,15 @@ require 'twilio-ruby'
 # and set the environment variables. See http://twil.io/secure
 account_sid = ENV['TWILIO_ACCOUNT_SID']
 auth_token = ENV['TWILIO_AUTH_TOKEN']
+
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-fax = @client.fax.faxes
+call = @client.calls
   .create(
-     from: '+15017122661',
-     to: '+15558675310',
-     media_url: 'https://www.twilio.com/docs/documents/25/justthefaxmaam.pdf'
+     machine_detection: 'Enable',
+     url: 'https://handler.twilio.com/twiml/EH8ccdbd7f0b8fe34357da8ce87ebe5a16',
+     to: '+1562300000',
+     from: '+18180000000'
    )
 
-puts fax.sid
+puts call.sid
