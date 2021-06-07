dd3ef21dd0087bad8b0d0ad16dfff76684780afb
Kyle Crews <dev.kylecrews@gmail.com>
2021-06-07 17:15:50 -0500

    Added class and method

--- run_this.rb
+++ run_this.rb
@@ -2,26 +2,30 @@
 require 'rubygems'
 require 'twilio-ruby'
 
-# Find your Account SID and Auth Token at twilio.com/console ... add to the strings below
-account_sid = ''
-auth_token = ''
+class Call
 
-=begin
-# and set the environment variables. See http://twil.io/secure
-account_sid = ENV['TWILIO_ACCOUNT_SID']
-auth_token = ENV['TWILIO_AUTH_TOKEN']
-=end
+  def self.make_call
 
-# initiates the connection to Twilio
-@client = Twilio::REST::Client.new(account_sid, auth_token)
+    # Find your Account SID and Auth Token at twilio.com/console ... add to the strings below
+    account_sid = ''
+    auth_token = ''
 
-# creates a call with Answering Machine Destection enabled (https://www.twilio.com/docs/voice/answering-machine-detection)
-call = @client.calls
-  .create(
-     machine_detection: 'Enable',
-     url: 'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Calls.json',
-     to: '+18553922666',
-     from: '{your_twilio_number}'
-   )
-# outputs the number that was called, if request is successful
-puts 'Successfully made a call to ' + call.to
+    # initiates the connection to Twilio
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+    # creates a call with Answering Machine Destection enabled (https://www.twilio.com/docs/voice/answering-machine-detection)
+    call = @client.calls
+      .create(
+        machine_detection: 'Enable',
+        url: 'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Calls.json',
+        to: '+18553922666',
+        from: '{your_twilio_number}'
+      )
+    # outputs the number that was called, if request is successful
+    puts 'Successfully made a call to ' + call.to
+
+  end
+
+  self.make_call
+
+end
