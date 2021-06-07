aad82bda18e1becb240cf27c114a362c97690802
Kyle Crews <dev.kylecrews@gmail.com>
2021-06-07 18:37:11 -0500

    added functionality and comments to run_this.rb

--- run_this.rb
+++ run_this.rb
@@ -1,10 +1,24 @@
 # Download the helper library from https://www.twilio.com/docs/ruby/install
 require 'rubygems'
 require 'twilio-ruby'
+require 'pry'
 
 class Call
 
-  def self.make_call
+  # add fax numbers to array. Format needs to be like this: ['+18553922666', '+18507020366', '+18504314447', '+18508776674']
+  @fax_numbers = []
+
+  # iterates over the fax_numbers array, calling the make_call() method each pass
+  def self.start
+
+      @fax_numbers.each do |number|
+          self.make_call(number)
+      end
+
+  end
+
+  # makes a call via the API with Machine Detection enabled... outputs number called.
+  def self.make_call(number)
 
     # Find your Account SID and Auth Token at twilio.com/console ... add to the strings below
     account_sid = ''
@@ -17,15 +31,16 @@ class Call
     call = @client.calls
       .create(
         machine_detection: 'Enable',
-        url: 'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Calls.json',
-        to: '+18553922666',
-        from: '{your_twilio_number}'
+        url: 'https://api.twilio.com/2010-04-01/Accounts/{account_sid}/Calls.json', # << add your twilio account sid >>
+        to: "#{number}",
+        from: '{your_twilio_number}' # << add your twilio phone number >>
       )
+
     # outputs the number that was called, if request is successful
     puts 'Successfully made a call to ' + call.to
 
   end
 
-  self.make_call
+  self.start
 
 end
